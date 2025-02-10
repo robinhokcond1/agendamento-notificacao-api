c6327c441c89912d40bc8e65b0d917de0d6f68a6
Robinhokcond1 <robinhokcond1@hotmail.com>
2025-02-10 19:29:42 -0300

    adicionado métodos de buscas de notificações.

---
+++ src/main/java/com/javanauta/agendamento_notificacao_api/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.javanauta.agendamento_notificacao_api.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
